48115d496a940b303e049605fc5175984504a8b7
lokapradip <lokapradipselvakumar@gmail.com>
2024-08-06 22:33:43 +0530

    Add files via upload

---
+++ password Generator.py
@@ -0,0 +1,13 @@
+
+import random                       
+def generate_password(len):  
+    "This function accepts a parameter 'len' and returns a randomly generated password"  
+    list_of_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
+    pass_str = ""  
+    for i in range(len):  
+        pass_str = pass_str + random.choice(list_of_chars)  
+    return pass_str  
+if __name__ == "__main__":  
+    len = 8  
+    pass_str = generate_password(len)  
+    print("A randomly generated Password is:", pass_str)  
